6f072ebdb6ab55bc0d3136b6af07b1d5219077a8
HARSH SHARMA <harsh159357@gmail.com>
2020-08-09 04:20:16 +0530

    RemoveDuplicates

---
+++ src/com/harsh/sharma/leetcode/easy/RemoveDuplicates.kt
@@ -0,0 +1,26 @@
+package com.harsh.sharma.leetcode.easy
+
+/*
+        26. Remove Duplicates from Sorted Array
+
+        https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ */
+
+fun removeDuplicates(nums: IntArray): Int {
+    if (nums.isEmpty()) return 0
+
+    var i = 0
+    for (j in 1 until nums.size) {
+        if (nums[j] != nums[i]) {
+            i++
+            nums[i] = nums[j]
+        }
+    }
+    return i + 1
+}
+
+fun main() {
+    println(removeDuplicates(intArrayOf(0, 0, 1, 1, 1, 2, 2, 3, 3, 4)))
+    println(removeDuplicates(intArrayOf(1, 1, 2)))
+    println(removeDuplicates(intArrayOf(1,1,2,2,3,3)))
+}
