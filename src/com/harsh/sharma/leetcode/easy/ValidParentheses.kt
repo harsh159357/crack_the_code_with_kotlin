8a036ae4b95c3c245f7cce5b6e0ee59e11b7999d
HARSH SHARMA <harsh159357@gmail.com>
2020-08-08 22:05:33 +0530

    ValidParentheses

---
+++ src/com/harsh/sharma/leetcode/easy/ValidParentheses.kt
@@ -0,0 +1,41 @@
+package com.harsh.sharma.leetcode.easy
+
+import java.util.*
+
+/*
+
+        20. Valid Parentheses
+        https://leetcode.com/problems/valid-parentheses/
+
+ */
+
+fun isValid(s: String): Boolean {
+
+    val mappings: MutableMap<Char, Char> = mutableMapOf(
+        ')' to '(',
+        '}' to '{',
+        ']' to '['
+    )
+
+    val stack = Stack<Char>()
+    for (i in s.indices) {
+        val c = s[i]
+        if (mappings.containsKey(c)) {
+            val topElement = if (stack.empty()) '#' else stack.pop()
+            if (topElement != mappings[c]) {
+                return false
+            }
+        } else {
+            stack.push(c)
+        }
+    }
+    return stack.isEmpty()
+}
+
+fun main() {
+    println(if (isValid("()")) "Valid" else "Not Valid")
+    println(if (isValid("()[]{}")) "Valid" else "Not Valid")
+    println(if (isValid("(]")) "Valid" else "Not Valid")
+    println(if (isValid("([)]")) "Valid" else "Not Valid")
+    println(if (isValid("{[]}")) "Valid" else "Not Valid")
+}
