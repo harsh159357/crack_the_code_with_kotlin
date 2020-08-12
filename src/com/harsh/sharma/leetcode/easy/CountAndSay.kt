5ca614bb082ec20a5283cf267fcc334170fbbd32
HARSH SHARMA <harsh159357@gmail.com>
2020-08-12 23:53:31 +0530

    CountAndSay

---
+++ src/com/harsh/sharma/leetcode/easy/CountAndSay.kt
@@ -0,0 +1,44 @@
+package com.harsh.sharma.leetcode.easy
+
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+/*
+
+    38. Count and Say
+
+    https://leetcode.com/problems/count-and-say/
+
+ */
+
+fun countAndSay(n: Int): String {
+    var curr = StringBuilder("1")
+    var prev: StringBuilder
+    var count: Int
+    var say: Char
+    for (i in 1 until n) {
+        prev = curr
+        curr = StringBuilder()
+        count = 1
+        say = prev[0]
+        var j = 1
+        val len = prev.length
+        while (j < len) {
+            if (prev[j] != say) {
+                curr.append(count).append(say)
+                count = 1
+                say = prev[j]
+            } else count++
+            j++
+        }
+        curr.append(count).append(say)
+    }
+    return curr.toString()
+}
+
+fun main() {
+    for (i in 0..5) {
+        val randomInput = Random.nextInt(1..30)
+        println("Random Input:\t$randomInput\nOutput:\t${countAndSay(randomInput)}\n")
+    }
+}
