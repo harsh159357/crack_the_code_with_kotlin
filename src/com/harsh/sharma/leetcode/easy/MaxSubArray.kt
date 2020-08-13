dce9de7faaaf00185a89eff0579d63404c33e021
HARSH SHARMA <harsh159357@gmail.com>
2020-08-13 23:51:31 +0530

    MaxSubArray

---
+++ src/com/harsh/sharma/leetcode/easy/MaxSubArray.kt
@@ -0,0 +1,24 @@
+package com.harsh.sharma.leetcode.easy
+
+/*
+
+    53. Maximum Subarray
+
+    https://leetcode.com/problems/maximum-subarray/
+
+ */
+
+
+fun maxSubArray(nums: IntArray): Int {
+    var maxSoFar = nums[0]
+    var maxEndingHere = nums[0]
+    for (i in 1..nums.size-1) {
+        maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i])
+        maxSoFar = Math.max(maxSoFar, maxEndingHere)
+    }
+    return maxSoFar
+}
+
+fun main() {
+    println(maxSubArray(intArrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4)))
+}
