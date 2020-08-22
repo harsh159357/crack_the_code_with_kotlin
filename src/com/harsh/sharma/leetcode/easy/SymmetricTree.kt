4dd166ac57a04d7b4ef21283d6e6285da8e14284
HARSH SHARMA <harsh159357@gmail.com>
2020-08-22 17:40:00 +0530

    Symmetric Tree

---
+++ src/com/harsh/sharma/leetcode/easy/SymmetricTree.kt
@@ -0,0 +1,23 @@
+package com.harsh.sharma.leetcode.easy
+
+import com.harsh.sharma.leetcode.util.TreeNode
+
+
+/*
+
+    101. Symmetric Tree
+
+    https://leetcode.com/problems/symmetric-tree/
+
+ */
+
+fun isSymmetric(root: TreeNode?): Boolean {
+    return isMirror(root, root)
+}
+
+fun isMirror(t1: TreeNode?, t2: TreeNode?): Boolean {
+    if (t1 == null && t2 == null) return true
+    return if (t1 == null || t2 == null) false else t1.`val` == t2.`val`
+            && isMirror(t1.right, t2.left)
+            && isMirror(t1.left, t2.right)
+}
