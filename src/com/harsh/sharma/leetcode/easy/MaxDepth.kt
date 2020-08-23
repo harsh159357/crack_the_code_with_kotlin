f619cfcfce0a8e920f5477b6cd8d0477dfecae8a
HARSH SHARMA <harsh159357@gmail.com>
2020-08-23 19:04:20 +0530

    MaxDepth

---
+++ src/com/harsh/sharma/leetcode/easy/MaxDepth.kt
@@ -0,0 +1,20 @@
+package com.harsh.sharma.leetcode.easy
+
+import com.harsh.sharma.leetcode.util.TreeNode
+
+/*
+
+    104. Maximum Depth of Binary Tree
+
+    https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+ */
+
+fun maxDepth(root: TreeNode?): Int {
+    if (root == null) {
+        return 0;
+    }
+
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+}
+
