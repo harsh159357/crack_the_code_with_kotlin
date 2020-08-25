f835258151c1e890c0ad418e10d4c4e2fa71cd74
HARSH SHARMA <harsh159357@gmail.com>
2020-08-25 08:20:58 +0530

    SortedArrayToBST

---
+++ src/com/harsh/sharma/leetcode/easy/SortedArrayToBST.kt
@@ -0,0 +1,27 @@
+package com.harsh.sharma.leetcode.easy
+
+import com.harsh.sharma.leetcode.util.TreeNode
+
+/*
+
+    108. Convert Sorted Array to Binary Search Tree
+
+    https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+
+ */
+
+
+
+fun sortedArrayToBST(nums: IntArray): TreeNode? {
+    return bstBuilder(nums, 0, nums.size - 1)
+}
+
+fun bstBuilder(nums: IntArray, left: Int, right: Int): TreeNode? {
+    if (left > right) return null
+    val mid = (left + right + 1) / 2
+    val root = TreeNode(nums[mid])
+    if (left == right) return root
+    root.left = bstBuilder(nums, left, mid - 1)
+    root.right = bstBuilder(nums, mid + 1, right)
+    return root
+}
