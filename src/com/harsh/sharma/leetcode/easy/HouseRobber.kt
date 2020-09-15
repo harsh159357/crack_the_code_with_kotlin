3ddec4875d5d35019d68245b20663453183de265
HARSH SHARMA <harsh159357@gmail.com>
2020-09-15 21:19:05 +0530

    HouseRobber

---
+++ src/com/harsh/sharma/leetcode/easy/HouseRobber.kt
@@ -0,0 +1,47 @@
+package com.harsh.sharma.leetcode.easy
+
+import kotlin.math.max
+
+/*
+
+    198. House Robber
+
+    https://leetcode.com/problems/house-robber/
+
+ */
+
+fun robOne(nums: IntArray): Int {
+    var prev: Int = 0
+    var curr: Int = 0
+
+    for (n in nums) {
+        val temp: Int = curr
+        curr = max(prev + n, curr)
+        prev = temp
+    }
+    return curr
+}
+
+fun robTwo(nums: IntArray): Int {
+    if (nums.isEmpty()) return 0
+    val maxArray = IntArray(nums.size) { 0 }
+    for (i in nums.indices) {
+        val prev = if (i > 0) {
+            maxArray[i - 1]
+        } else {
+            0
+        }
+        val twoPrev = if (i > 1) {
+            maxArray[i - 2]
+        } else {
+            0
+        }
+        maxArray[i] = maxOf(nums[i] + twoPrev, prev)
+    }
+    return maxArray[nums.size - 1]
+}
+
+fun main() {
+    println(robOne(intArrayOf(1,2,3,1)))
+    println(robOne(intArrayOf(2,7,9,3,1)))
+}
