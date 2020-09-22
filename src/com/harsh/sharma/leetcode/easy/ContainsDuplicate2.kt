ca1e0e27f0ce72537b79fff031da7a35530c99ed
HARSH SHARMA <harsh159357@gmail.com>
2020-09-22 21:53:32 +0530

    ContainsDuplicate2

---
+++ src/com/harsh/sharma/leetcode/easy/ContainsDuplicate2.kt
@@ -0,0 +1,52 @@
+package com.harsh.sharma.leetcode.easy
+
+/*
+
+    219. Contains Duplicate II
+
+    https://leetcode.com/problems/contains-duplicate-ii/
+
+ */
+
+fun containsNearbyDuplicateOne(nums: IntArray, k: Int): Boolean {
+    val map = hashMapOf<Int, Int>()
+    for (i in nums.indices) {
+        if (map[nums[i]] != null && i - map[nums[i]]!! <= k) {
+            return true
+        }
+        map[nums[i]] = i
+    }
+    return false
+}
+
+fun containsNearbyDuplicateTwo(nums: IntArray, k: Int): Boolean {
+    val set = HashSet<Int>()
+    for (i in nums.indices) {
+        if (set.contains(nums[i])) return true
+        set.add(nums[i])
+        if (set.size > k) {
+            set.remove(nums[i - k])
+        }
+    }
+    return false
+}
+
+fun containsNearbyDuplicateWorst(nums: IntArray, k: Int): Boolean {
+    for (dp in 1..k) {
+        for (i in 0 until nums.size - dp) {
+            if (nums[i] == nums[i + dp]) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+fun main() {
+    println(containsNearbyDuplicateOne(intArrayOf(1, 2, 3, 1), 3))
+    println(containsNearbyDuplicateOne(intArrayOf(1, 0, 1, 1), 1))
+    println(containsNearbyDuplicateOne(intArrayOf(1, 2, 3, 1, 2, 3), 2))
+    println(containsNearbyDuplicateOne(intArrayOf(), 2))
+    println(containsNearbyDuplicateOne(intArrayOf(1, 1), 1))
+    println(containsNearbyDuplicateOne(intArrayOf(1, 1), 2))
+}
