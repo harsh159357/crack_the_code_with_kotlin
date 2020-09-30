ae0db3feddba8a3336680508d4b478e19454b793
HARSH SHARMA <harsh159357@gmail.com>
2020-09-30 23:57:32 +0530

    ValidAnagram

---
+++ src/com/harsh/sharma/leetcode/easy/ValidAnagram.kt
@@ -0,0 +1,45 @@
+package com.harsh.sharma.leetcode.easy
+
+import java.util.*
+
+
+/*
+
+    242. Valid Anagram
+
+    https://leetcode.com/problems/valid-anagram/
+
+ */
+
+fun isAnagramUsingSorting(s: String, t: String): Boolean {
+    if (s.length != t.length) {
+        return false
+    }
+    val str1 = s.toCharArray()
+    val str2 = t.toCharArray()
+    Arrays.sort(str1)
+    Arrays.sort(str2)
+    return Arrays.equals(str1, str2)
+}
+
+fun isAnagram(s: String, t: String): Boolean {
+    if (s.length != t.length) {
+        return false
+    }
+    val table = IntArray(26)
+    for (element in s) {
+        table[element - 'a']++
+    }
+    for (i in t.indices) {
+        table[t[i] - 'a']--
+        if (table[t[i] - 'a'] < 0) {
+            return false
+        }
+    }
+    return true
+}
+
+fun main() {
+    println(isAnagram("anagram","nagaram"))
+    println(isAnagram("rat","car"))
+}
