6f8fcbe7f6fb7454e6b2996c53464a9610576cbd
HARSH SHARMA <harsh159357@gmail.com>
2020-10-16 23:34:20 +0530

    IsPerfectSquare

---
+++ src/com/harsh/sharma/leetcode/easy/ValidPerfectSquare.kt
@@ -0,0 +1,56 @@
+package com.harsh.sharma.leetcode.easy
+
+/*
+
+    367. Valid Perfect Square
+
+    https://leetcode.com/problems/valid-perfect-square/
+
+ */
+
+fun isPerfectSquareOne(num: Int): Boolean {
+    if (num == 1) return true
+
+    var left = 1.toBigInteger()
+    var right = num.toBigInteger()
+    val bigNum = num.toBigInteger()
+
+    while (right - left > 1.toBigInteger()) {
+        val current = (right + left) / 2.toBigInteger()
+        val sq = current * current
+
+        if (sq == bigNum) return true
+        if (sq < bigNum) left = current
+        if (sq > bigNum) right = current
+    }
+
+    return false
+}
+
+
+fun isPerfectSquareTwo(num: Int): Boolean {
+    if(num == 1) return true
+    var low = 0
+    var high = num
+    var mid = 0
+    for (i in 0..10000) {
+        mid = (high - low) / 2 + low
+        if (mid == 0) return false
+        val square = mid*mid
+        when {
+            willOverflow(mid.toLong()) -> high = mid
+            square == num -> return true
+            square > num -> high = mid
+            else -> low = mid
+        }
+    }
+    return false
+}
+fun willOverflow(x:Long) : Boolean =
+    x*x > Int.MAX_VALUE
+
+fun main() {
+    println(isPerfectSquareOne(16))
+    println(isPerfectSquareOne(14))
+    println(isPerfectSquareOne(25))
+}
