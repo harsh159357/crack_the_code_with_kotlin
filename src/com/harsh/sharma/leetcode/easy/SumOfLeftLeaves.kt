891b284c3f00b4220b57c5b5624df4b3067ef4e0
HARSH SHARMA <harsh159357@gmail.com>
2020-10-23 21:55:09 +0530

    SumOfLeftLeaves

---
+++ src/com/harsh/sharma/leetcode/easy/SumOfLeftLeaves.kt
@@ -0,0 +1,27 @@
+package com.harsh.sharma.leetcode.easy
+
+import com.harsh.sharma.leetcode.util.TreeNode
+
+/*
+
+    404. Sum of Left Leaves
+
+    https://leetcode.com/problems/sum-of-left-leaves/
+
+ */
+
+private fun isLeaf(node: TreeNode) = node.left != null && node.left?.left == null && node.left?.right == null
+
+fun sumOfLeftLeaves(root: TreeNode?): Int {
+    var count = 0
+    if (root == null)
+        return count
+
+    count += when (isLeaf(root)) {
+        true -> root.left?.`val` ?: 0
+        false -> sumOfLeftLeaves(root.left)
+    }
+
+    count += sumOfLeftLeaves(root.right);
+    return count
+}
