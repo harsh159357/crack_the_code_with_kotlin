7ac9af9582c1eade4be0e778dc3b9d70f98bc3b5
HARSH SHARMA <harsh159357@gmail.com>
2020-10-27 23:21:16 +0530

    ThirdMaximumNumber

--- src/com/harsh/sharma/leetcode/easy/FizzBuzz.kt
+++ src/com/harsh/sharma/leetcode/easy/FizzBuzz.kt
@@ -34,3 +34,66 @@ fun fizzBuzzNaive(n: Int): List<String>? {
     }
     return ans
 }
+
+fun fizzBuzzStringConcatenation(n: Int): List<String>? {
+    // ans list
+    val ans: MutableList<String> = ArrayList()
+    for (num in 1..n) {
+        val divisibleBy3 = num % 3 == 0
+        val divisibleBy5 = num % 5 == 0
+        var numAnsStr = ""
+        if (divisibleBy3) {
+            // Divides by 3, add Fizz
+            numAnsStr += "Fizz"
+        }
+        if (divisibleBy5) {
+            // Divides by 5, add Buzz
+            numAnsStr += "Buzz"
+        }
+        if (numAnsStr == "") {
+            // Not divisible by 3 or 5, add the number
+            numAnsStr += num.toString()
+        }
+
+        // Append the current answer str to the ans list
+        ans.add(numAnsStr)
+    }
+    return ans
+}
+
+fun fizzBuzz(n: Int): List<String?>? {
+
+    // ans list
+    val ans: MutableList<String> = ArrayList()
+
+    // Hash map to store all fizzbuzz mappings.
+    val fizzBizzDict: HashMap<Int, String> = object : HashMap<Int, String>() {
+        init {
+            put(3, "Fizz")
+            put(5, "Buzz")
+        }
+    }
+    for (num in 1..n) {
+        var numAnsStr: String? = ""
+        for (key in fizzBizzDict.keys) {
+
+            // If the num is divisible by key,
+            // then add the corresponding string mapping to current numAnsStr
+            if (num % key == 0) {
+                numAnsStr += fizzBizzDict[key]
+            }
+        }
+        if (numAnsStr == "") {
+            // Not divisible by 3 or 5, add the number
+            numAnsStr += num.toString()
+        }
+
+        // Append the current answer str to the ans list
+        ans.add(numAnsStr.toString())
+    }
+    return ans
+}
+
+fun main() {
+    println(fizzBuzz(15))
+}
---
+++ src/com/harsh/sharma/leetcode/easy/ThirdMaximumNumber.kt
@@ -0,0 +1,55 @@
+package com.harsh.sharma.leetcode.easy
+
+import java.util.*
+
+/*
+
+    414. Third Maximum Number
+
+    https://leetcode.com/problems/third-maximum-number/
+
+ */
+
+fun thirdMax(nums: IntArray): Int {
+    val maxHeap = PriorityQueue(nums.size, compareByDescending<Int> { it })
+    nums.toSet().forEach {
+        maxHeap.add(it)
+    }
+    if (maxHeap.size < 3) {
+        return maxHeap.poll()
+    }
+    maxHeap.poll()
+    maxHeap.poll()
+    return maxHeap.poll()
+}
+
+fun thirdMax2(nums: IntArray): Int {
+    val map = mutableMapOf<Int, Int>()
+    var max0 = Integer.MIN_VALUE
+    var max1 = Integer.MIN_VALUE
+    var max2 = Integer.MIN_VALUE
+    nums.forEach {
+        map[it] = it
+        when {
+            it > max0 -> {
+                max2 = max1
+                max1 = max0
+                max0 = it
+            }
+            it > max1 && it != max0 -> {
+                max2 = max1
+                max1 = it
+            }
+            it > max2 && it != max1 && it != max0 -> {
+                max2 = it
+            }
+        }
+    }
+    return if (map.size < 3) max0 else max2
+}
+
+fun main() {
+    println(thirdMax2(intArrayOf(3, 2, 1)))
+    println(thirdMax2(intArrayOf(1, 2)))
+    println(thirdMax2(intArrayOf(2, 2, 3, 1)))
+}
