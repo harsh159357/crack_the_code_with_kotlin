b0e79a42bcc192484fe64a25824a1457972ed69c
HARSH SHARMA <harsh159357@gmail.com>
2020-10-31 23:42:21 +0530

    FindAllNumbersDisappearedInAnArray

---
+++ src/com/harsh/sharma/leetcode/easy/FindAllNumbersDisappearedInAnArray.kt
@@ -0,0 +1,38 @@
+package com.harsh.sharma.leetcode.easy
+
+/*
+
+    448. Find All Numbers Disappeared in an Array
+
+    https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+ */
+
+fun findDisappearedNumbersOne(nums: IntArray): List<Int> {
+    var i = 0
+    while (i < nums.size) {
+        if (nums[i] != nums[nums[i] - 1]) {
+            swap(nums, i, nums[i] - 1)
+            --i
+        }
+        i++
+    }
+    return (1..nums.size).filter { nums[it - 1] != it }.toList()
+}
+
+private fun swap(nums: IntArray, x: Int, y: Int) {
+    nums[x] += nums[y]
+    nums[y] = nums[x] - nums[y]
+    nums[x] -= nums[y]
+}
+
+fun findDisappearedNumbers(nums: IntArray): List<Int> {
+    nums.indices.forEach { i ->
+        nums[(nums[i] - 1) % nums.size] += nums.size
+    }
+    return (1..nums.size).filter { nums[it-1] <= nums.size }.toList()
+}
+
+fun main() {
+    println(findDisappearedNumbers(intArrayOf(4,3,2,7,8,2,3,1)))
+}
