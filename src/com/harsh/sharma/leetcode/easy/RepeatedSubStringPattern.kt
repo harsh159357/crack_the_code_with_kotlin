4a31822898c9b2b1f442c33932dd33209f72b823
HARSH SHARMA <harsh159357@gmail.com>
2020-11-03 01:47:18 +0530

    RepeatedSubStringPattern

---
+++ src/com/harsh/sharma/leetcode/easy/RepeatedSubStringPattern.kt
@@ -0,0 +1,41 @@
+package com.harsh.sharma.leetcode.easy
+
+/*
+
+    459. Repeated Substring Pattern
+
+    https://leetcode.com/problems/repeated-substring-pattern/
+
+ */
+
+fun repeatedSubstringPatternOne(s: String): Boolean {
+    val sLen = s.length
+    for (i in 0 until sLen / 2) {
+        val substring = s.substring(0, i + 1)
+        if (sLen % substring.length == 0) {
+            var repetitions = sLen / substring.length
+            val builder = StringBuilder()
+            while (repetitions > 0) {
+                builder.append(substring)
+                repetitions--
+            }
+
+            if (builder.toString() == s) return true
+        }
+    }
+    return false
+}
+
+fun repeatedSubstringPattern(s: String): Boolean {
+    var str = s.plus(s)
+    str = str.drop(1)
+    str = str.dropLast(1)
+    return str.contains(s)
+}
+
+
+fun main() {
+    println(repeatedSubstringPattern("abab"))
+    println(repeatedSubstringPattern("aba"))
+    println(repeatedSubstringPattern("abcabcabcabc"))
+}
