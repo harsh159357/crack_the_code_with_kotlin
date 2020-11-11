82d1166d67f694f9931b4c7e273950aa7c1a940a
HARSH SHARMA <harsh159357@gmail.com>
2020-11-11 22:40:33 +0530

    KeyboardRow

---
+++ src/com/harsh/sharma/leetcode/easy/KeyboardRow.kt
@@ -0,0 +1,32 @@
+package com.harsh.sharma.leetcode.easy
+
+/*
+
+    500. Keyboard Row
+
+    https://leetcode.com/problems/keyboard-row/
+
+ */
+
+fun findWords(words: Array<String>): Array<String> {
+    val kr1 = hashSetOf('q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p')
+    val kr2 = hashSetOf('a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l')
+    val kr3 = hashSetOf('z', 'x', 'c', 'v', 'b', 'n', 'm')
+
+    val result = ArrayList<String>()
+    words.forEach {
+
+        when {
+            kr1.containsAll(it.toLowerCase().toHashSet()) ||
+                    kr2.containsAll(it.toLowerCase().toHashSet()) ||
+                    kr3.containsAll(it.toLowerCase().toHashSet()) -> result.add(it)
+        }
+    }
+    return result.toTypedArray()
+}
+
+fun main() {
+    println(findWords(arrayOf("Hello","Alaska","Dad","Peace")).forEach {
+        print("$it\t")
+    })
+}
