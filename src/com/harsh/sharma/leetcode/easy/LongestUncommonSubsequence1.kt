dce81a7dc3eb2b8d95c5bc210cfe00137ae73b25
HARSH SHARMA <harsh159357@gmail.com>
2020-11-18 21:41:23 +0530

    LongestUncommonSubsequence1

---
+++ src/com/harsh/sharma/leetcode/easy/LongestUncommonSubsequence1.kt
@@ -0,0 +1,21 @@
+package com.harsh.sharma.leetcode.easy
+
+import com.harsh.sharma.leetcode.util.printAndNewLine
+
+/*
+
+    521. Longest Uncommon Subsequence I
+
+    https://leetcode.com/problems/longest-uncommon-subsequence-i/
+
+ */
+
+fun findLUSlength(a: String, b: String): Int {
+    return if (a == b) -1 else a.length.let { if (it > b.length) it else b.length }
+}
+
+fun main() {
+    findLUSlength("aba", "cdc").printAndNewLine()
+    findLUSlength("aaa", "bbb").printAndNewLine()
+    findLUSlength("aaa", "aaa").printAndNewLine()
+}
