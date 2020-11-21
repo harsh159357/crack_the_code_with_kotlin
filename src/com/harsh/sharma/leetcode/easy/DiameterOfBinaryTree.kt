19e97546b6110c931d2801db34dc3665c702bf58
HARSH SHARMA <harsh159357@gmail.com>
2020-11-21 22:12:47 +0530

    DiameterOfBinaryTree

---
+++ src/com/harsh/sharma/leetcode/easy/DiameterOfBinaryTree.kt
@@ -0,0 +1,27 @@
+package com.harsh.sharma.leetcode.easy
+
+import com.harsh.sharma.leetcode.util.TreeNode
+import kotlin.math.max
+
+/*
+
+    543. Diameter of Binary Tree
+
+    https://leetcode.com/problems/diameter-of-binary-tree/
+
+ */
+
+var ans = 0
+fun diameterOfBinaryTree(root: TreeNode?): Int {
+    ans = 1
+    depth(root)
+    return ans - 1
+}
+
+fun depth(node: TreeNode?): Int {
+    if (node == null) return 0
+    val l = depth(node.left)
+    val r = depth(node.right)
+    ans = max(ans, l + r + 1)
+    return max(l, r) + 1
+}
