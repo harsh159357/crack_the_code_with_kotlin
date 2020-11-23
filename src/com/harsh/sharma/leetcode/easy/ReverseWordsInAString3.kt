e34b77b946c5811f25fa9c0ef0ffb6c6048f7775
HARSH SHARMA <harsh159357@gmail.com>
2020-11-23 11:06:21 +0530

    ReverseWordsInAString3

---
+++ src/com/harsh/sharma/leetcode/easy/ReverseWordsInAString3.kt
@@ -0,0 +1,40 @@
+package com.harsh.sharma.leetcode.easy
+
+import com.harsh.sharma.leetcode.util.printAndNewLine
+
+/*
+
+    557. Reverse Words in a String III
+
+    https://leetcode.com/problems/reverse-words-in-a-string-iii/
+
+ */
+
+fun reverseWords(s: String): String {
+    val array = s.toCharArray()
+    var s = 0
+    var e = 0
+    fun reverse(l: Int, r: Int) {
+        var l = l
+        var r = r
+        while (l < r && r < array.size) {
+            array[l] = array[r].also { array[r] = array[l] }
+            l++.also { r-- }
+        }
+    }
+
+
+    while (e < array.size) {
+        if (array[e] == ' ') {
+            reverse(s, e - 1)
+            s = e + 1
+        }
+        e++
+    }
+    reverse(s, e - 1)
+    return String(array)
+}
+
+fun main() {
+    reverseWords("Let's take LeetCode contest").printAndNewLine()
+}
