4a67f137d285ecfadf69d3383bb6c0624d6f0f87
HARSH SHARMA <harsh159357@gmail.com>
2020-11-25 23:41:46 +0530

    ArrayPartition1

---
+++ src/com/harsh/sharma/leetcode/easy/ArrayPartition1.kt
@@ -0,0 +1,19 @@
+package com.harsh.sharma.leetcode.easy
+
+import com.harsh.sharma.leetcode.util.printAndNewLine
+
+/*
+
+    561. Array Partition I
+
+    https://leetcode.com/problems/array-partition-i/
+
+ */
+
+fun arrayPairSum(nums: IntArray): Int {
+    return nums.sortedArray().filterIndexed { index, _ -> index % 2 == 0 }.sum()
+}
+
+fun main() {
+    arrayPairSum(intArrayOf(1, 4, 3, 2)).printAndNewLine()
+}
